6590be0cfb0d924a27f1f6a872b1169550a5b9f2
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-09-28 18:14:45 -0300

    requisito 3 concluído

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+project = Queue()
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    for el in instance.queue:
+        if path_file == el["nome_do_arquivo"]:
+            return
+
+    new_format = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(new_format)
+    print(new_format)
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    response = instance.dequeue()
+    if response is None:
+        return print("Não há elementos")
+    return print(f'Arquivo {response["nome_do_arquivo"]} removido com sucesso')
 
 
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+def file_metadata(instance: Queue, position):
+    try:
+        return print(instance.search(position))
+    except IndexError:
+        return print("Posição inválida", file=sys.stderr)
+
+
+process("statics/arquivo_teste.txt", project)
