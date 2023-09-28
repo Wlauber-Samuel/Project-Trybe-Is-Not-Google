9be334ea56ad2e48c9a637fd6e64114434f82910
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-09-28 20:41:12 -0300

    requisitos 7 e 8 concluídos

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,32 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    response = list()
+    ocorrencia = list()
+    for el in instance.queue:
+        for ind in range(len(el["linhas_do_arquivo"])):
+            if word.lower() in el['linhas_do_arquivo'][ind].lower():
+                ocorrencia.append({'linha': ind + 1})
+        if len(ocorrencia) > 0:
+            response.append({
+                "palavra": word,
+                "arquivo": el['nome_do_arquivo'],
+                "ocorrencias": ocorrencia})
+    return response
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = list()
+    ocorrencia = list()
+    for el in instance.queue:
+        for ind in range(len(el["linhas_do_arquivo"])):
+            if word.lower() in el['linhas_do_arquivo'][ind].lower():
+                ocorrencia.append({'linha': ind + 1,
+                                   'conteudo': el['linhas_do_arquivo'][ind]})
+        if len(ocorrencia) > 0:
+            response.append({
+                "palavra": word,
+                "arquivo": el['nome_do_arquivo'],
+                "ocorrencias": ocorrencia})
+    return response
