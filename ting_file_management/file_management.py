b4c29a287552e3d256670aefb86f70a3ef1fa8c6
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-09-28 18:04:18 -0300

    requisito 2 concluído

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-# initial commit
+    if path_file[-3:] == "txt":
+        try:
+            with open(path_file, mode="r", encoding="UTF-8") as file:
+                content = file.read().split("\n")
+                return content
+        except FileNotFoundError:
+            error_message = f"Arquivo {path_file} não encontrado"
+            return print(error_message, file=sys.stderr)
+    return print("Formato inválido", file=sys.stderr)
